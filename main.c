c552e20d53b8d949d3edb975a04f06e8163cc2d9
NikitaBolsunovskiy <41104234+NikitaBolsunovskiy@users.noreply.github.com>
2018-07-17 10:33:38 +0300

    test Data

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 
 #define MaxN 100
 
+void solution1();
+
 void print(int N, int*a){
     int i;
     for (i=0; i<N; i++)
@@ -9,11 +11,44 @@ void print(int N, int*a){
     printf("\n");
 }
 
-void bubleSort(int N, int *a){
+void swap(int *a, int *b){
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
 
+int bubleSort(int N, int *a){
+
+    // counter - переменная, в которой будем хранить количество операций, подсчет не точный, просто, чтобы был понятен порядок цифр...
+
+    int counter = 0;
+
+    for(int i = 0; i<N;i++){
+        counter += 1;
+        for (int j = 0; j < N-1; ++j) {
+            counter += 1;
+            if (a[j]>a[j+1]){
+                swap(&a[j],&a[j+1]);
+                counter+=1;
+            }
+        }
+    }
+
+    return counter;
 }
 
 int main (int argc, char* argv[]){
+
+    solution1();
+
+    return 0;
+}
+
+void solution1(){
+
+//    1. Попробовать оптимизировать пузырьковую сортировку. Сравнить количество операций сравнения
+//    оптимизированной и не оптимизированной программы. Написать функции сортировки, которые
+//    возвращают количество операций.
 
     int a[MaxN];
 
@@ -27,11 +62,12 @@ int main (int argc, char* argv[]){
         fscanf(in,"%i",&a[i]);
     }
 
-    puts("Массив до сортировки:");
-
-    print(N,a);
-
     fclose(in);
 
-    return 0;
+    puts("Массив до сортировки:");
+    print(N,a);
+    int counter = bubleSort(N,a);
+    puts("Массив после сортировки:");
+    print(N,a);
+    printf("Количество операций: %d\n",counter);
 }
