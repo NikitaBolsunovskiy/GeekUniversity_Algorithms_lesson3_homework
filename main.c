be452d62ae7cf9a42c65062b9e369da2c66859ab
NikitaBolsunovskiy <41104234+NikitaBolsunovskiy@users.noreply.github.com>
2018-07-17 11:04:41 +0300

    Solution 1 ready

--- main.c
+++ main.c
@@ -17,6 +17,19 @@ void swap(int *a, int *b){
     *b = t;
 }
 
+void readArrayFromTxtFile(int * N, int * a, char path[]){
+
+    FILE *in;
+    in = fopen(path,"r");
+    int M = 0;
+    fscanf(in,"%i",&M);
+    for(int i = 0; i<M; i++){
+        fscanf(in,"%i",&a[i]);
+    }
+    fclose(in);
+    *N = M;
+}
+
 int bubleSort(int N, int *a){
 
     // counter - переменная, в которой будем хранить количество операций, подсчет не точный, просто, чтобы был понятен порядок цифр...
@@ -37,6 +50,43 @@ int bubleSort(int N, int *a){
     return counter;
 }
 
+int shakerSort(int N, int *a){
+
+    // counter - переменная, в которой будем хранить количество операций, подсчет не точный, просто, чтобы был понятен порядок цифр...
+
+    int counter = 0;
+    int min = 0;
+    int max = N-1;
+
+
+
+    for(int i = 0; i<N;i++){
+        counter += 1;
+        if(i%2==0){
+            for (int j = min; j < max; j++) {
+                counter+=1;
+                if(a[j]>a[j+1]){
+                    swap(&a[j],&a[j+1]);
+                    counter+=1;
+                }
+            }
+            max -= 1;
+        } else {
+            for (int j = max; j > min; j--) {
+                counter+=1;
+                if(a[j]<a[j-1]){
+                    swap(&a[j],&a[j-1]);
+                    counter+=1;
+                }
+            }
+            min+=1;
+        }
+        //print(N,a);
+    }
+
+    return counter;
+}
+
 int main (int argc, char* argv[]){
 
     solution1();
@@ -51,22 +101,22 @@ void solution1(){
 //    возвращают количество операций.
 
     int a[MaxN];
-
     int N;
 
-    FILE *in;
-    in = fopen("D:\\Java_projects_Study\\05_Algorithms_and_data_structures\\03\\homework3\\testData.txt","r");
-    fscanf(in,"%i",&N);
-
-    for(int i = 0; i<N; i++){
-        fscanf(in,"%i",&a[i]);
-    }
-
-    fclose(in);
+    readArrayFromTxtFile(&N,a,"D:\\Java_projects_Study\\05_Algorithms_and_data_structures\\03\\homework3\\testData.txt");
 
     puts("Массив до сортировки:");
     print(N,a);
     int counter = bubleSort(N,a);
+    puts("Массив после сортировки:");
+    print(N,a);
+    printf("Количество операций: %d\n",counter);
+
+    readArrayFromTxtFile(&N,a,"D:\\Java_projects_Study\\05_Algorithms_and_data_structures\\03\\homework3\\testData.txt");
+
+    puts("Массив до сортировки:");
+    print(N,a);
+    counter = shakerSort(N,a);
     puts("Массив после сортировки:");
     print(N,a);
     printf("Количество операций: %d\n",counter);
